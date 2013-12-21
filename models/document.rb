89f971bf97c8d2016bcf56be2eb765960cc3a277
John Holdun <john@johnholdun.com>
2013-12-21 16:13:19 -0500

    properly support deep file paths

--- models/document.rb
+++ models/document.rb
@@ -2,6 +2,7 @@ class Document
   include DataMapper::Resource
   property :id,         Serial
   property :user_id,    Integer
+  property :path,       String
   property :slug,       String
   property :type,       String
   property :created_at, DateTime
@@ -11,32 +12,24 @@ class Document
   attr_accessor :user_name
 
   def path= new_path
-    new_path.sub!(/^\//, '')
-    user_name, slug = new_path.split('/')
-    if slug.nil? # legacy
-      slug = user_name
-      user_name = nil
-    end
-    self.type = File.extname(slug)[1 .. -1] || 'html'
-    slug.sub!(/#{ File.extname(slug) }$/, '')
-    if user_name # legacy
-      self.user = User.first(name: user_name.downcase)
-      self.user_name = user_name if user.nil?
-    end
-    self.slug = slug
+    @path = new_path.sub /^\//, ''
+
+    user_name, *middle, filename = @path.split '/'
+
+    self.type = File.extname(filename)[1 .. -1] || 'html'
+    @path.sub! /\.html$/, ''
+
+    self.user = User.first(name: user_name.downcase)
+    self.user_name = user_name if user.nil?
+
+    self.slug = filename.sub(/\.#{ type }$/, '')
+    self[:path] = @path
+
     path
   end
 
   def title
     @title ||= @path.split('/').last
-  end
-
-  def path
-    @path ||= if user.nil? and user_name.nil? # legacy
-      "/#{ filename.sub /\.html$/, '' }"
-    else
-      "/#{ user.try(:name) || user_name }/#{ filename.sub /\.html$/, '' }"
-    end
   end
 
   def filename
