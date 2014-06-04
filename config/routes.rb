0f5dd08bc82ae64e02dc408996046b31f0d93408
John Holdun <john@johnholdun.com>
2014-06-03 22:04:48 -0400

    resources except hash wants controller methods, not http methods. also disable named route because it is erroring

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :posts, except: [:get, :patch]
+  resources :posts, except: :show
 
   get ':user' => 'users#show', as: :user
-  get '*path' => 'posts#show', as: :post
+  get '*path' => 'posts#show' #, as: :post
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
