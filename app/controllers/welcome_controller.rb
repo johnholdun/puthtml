a2d4ae6851229775fc399ca5a3431e5b7a9fa911
John Holdun <john@johnholdun.com>
2014-06-03 23:02:37 -0400

    use new instance variables on landing page for updated templates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @documents = Post.order('RANDOM()').limit(10)
+    @latest_documents = Post.order('created_at DESC').limit(10)
+    @greatest_documents = Post.order('views DESC').limit(10)
   end
 end
