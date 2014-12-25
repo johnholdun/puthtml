0c8c4361dda2b69673d47d6e9d085ae544958ed9
John Holdun <john@johnholdun.com>
2014-12-25 14:48:50 -0800

    Rails fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,42 @@
 class PostsController < ApplicationController
   before_filter :require_app_host, only: %w[edit destroy]
   before_filter :require_content_host, only: :show
-  before_filter :extract_file_info, only: [:create, :update]
+  before_filter :set_path_param
+
+  protect_from_forgery unless: Proc.new{ |c| c.params.is_a?(Hash) and c.params[:action] == 'create' and c.params.key? :api_key }
 
   def show
-    path = "#{ params[:path] }.#{ params[:format] || 'html' }"
-    @post = Post.find_by_path path
+    @post = Post.find_by_path params[:path]
     raise ActiveRecord::RecordNotFound unless @post
 
     render text: @post.contents, content_type: @post.content_type
   end
 
   def create
-    @post = current_user.posts.new post_params
+    if params.key? :api_key
+      params[:post] ||= {}
 
-    if @post.save then
-      redirect_to @post.path
+      %w[file path].each do |attr|
+        params[:post][attr] ||= params[attr] if params.key? attr
+      end
+
+      user = User.find_by_api_key params[:api_key]
+    else
+      user = current_user
+    end
+
+    @post = user.posts.new post_params
+
+    if @post.save
+      redirect_to "/#{ @post.path false }"
     else
       render :edit
     end
   end
 
   def edit
-    path = "#{ params[:path] }.#{ params[:format] || 'html' }"
+    path = params[:path].sub %r[^/edit-put/], ''
+
     @post = Post.find_by_path path
     raise ActiveRecord::RecordNotFound unless @post
 
@@ -34,7 +48,7 @@ class PostsController < ApplicationController
   def update
     @post = current_user.posts.find(params[:id])
 
-    if @post.update_attributes(post_params) then
+    if @post.update_attributes(post_params)
       redirect_to @post.path
     else
       render :edit
@@ -42,12 +56,12 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = current_user.posts.find(params[:id])
+    @post = current_user.posts.find_by_path params[:path]
 
     @post.destroy
-    flash.notice 'Post deleted.'
+    flash.notice = 'Post deleted.'
 
-    redirect_to current_user
+    redirect_to root_path
   end
 
   private
@@ -56,14 +70,10 @@ class PostsController < ApplicationController
     params.require(:post).permit(:file, :contents, :path, :apikey)
   end
 
-  def extract_file_info
-    if params[:file].is_a? Hash
-      tmpfile = params[:file][:tempfile]
-      # if this file is huge, don't read the whole thing--
-      # just enough to trigger the "too large" error
-      # (i'm not sure this actually gains us anything)
-      params[:contents] = open(tmpfile).read(Post::MAX_FILE_SIZE + 1)
-      params[:path] = params[:path].to_s.strip.present? ? params[:path].strip : params[:file][:filename]
-    end
+  def set_path_param
+    path = params[:path] || request.path
+    return true unless path.present?
+    path += '.html' unless path =~ /\.[^\/]+\/?$/
+    params[:path] = path
   end
 end
